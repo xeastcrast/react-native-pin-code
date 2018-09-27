33ed53519075923f142ab8fa395780b0b5b2efef
Worapol Buraphan <xeastcrast@hotmail.com>
2018-09-27 13:46:39 +0700

    Change input pin method && change style && add password svg

--- pin-code.js
+++ pin-code.js
@@ -1,256 +1,225 @@
-import React, {Component} from 'react';
-import {PropTypes} from 'prop-types';
-import {TextInput, View, Text} from 'react-native';
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { TextInput, View, Text, TouchableOpacity } from "react-native";
+import SVG, { Circle } from "react-native-svg";
 
-import {codePinStyles} from './pin-code-style';
+import { codePinStyles } from "./pin-code-style";
 
-class CodePin extends Component {
-  constructor(props) {
-    super(props);
+class PrettyPin extends Component {
+	constructor(props) {
+		super(props);
 
-    const codeLength = props.number || props.code.length;
+		const codeLength = props.number || props.code.length;
 
-    this.state = {
-      error: '',
-      number: codeLength,
-      code: new Array(codeLength).fill(''),
-      edit: null,
-      reset: false
-    };
+		this.state = {
+			error: "",
+			number: codeLength,
+			code: new Array(codeLength).fill(""),
+			edit: null,
+			reset: false,
+			value: ""
+		};
 
-    this.textInputsRefs = [];
+		this.clean = this.clean.bind(this);
+		this.focus = this.focus.bind(this);
+		this.handleEdit = this.handleEdit.bind(this);
+	}
 
-    this.clean = this.clean.bind(this);
-    this.focus = this.focus.bind(this);
-    this.isFocus = this.isFocus.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-    this.onKeyPress = this.onKeyPress.bind(this);
-  }
+	componentWillReceiveProps(newProps) {
+		const codeLength = newProps.number || newProps.code.length;
 
-  componentWillReceiveProps(newProps) {
-    const codeLength = newProps.number || newProps.code.length;
+		if (newProps.number !== this.props.number) {
+			this.setState({
+				number: codeLength,
+				edit: null
+			});
+		}
+	}
 
-    if (newProps.number !== this.props.number) {
-      this.setState({
-        number: codeLength,
-        edit: null
-      });
-    }
-  }
+	clean() {
+		this.setState(prevState => {
+			return {
+				code: new Array(prevState.number).fill(""),
+				reset: true,
+				value: ''
+			};
+		});
+		this.focus();
+	}
 
-  clean() {
-    this.setState(prevState => {
-      return {
-        code: new Array(prevState.number).fill(''),
-        edit: null,
-        reset: true
-      };
-    });
-    this.focus(0);
-  }
+	focus() {
+		// Check to ensure that input exists. This is important in the case of autofill.
+		if (this.textInputsRefs) this.textInputsRefs.focus();
+	}
 
-  focus(id) {
-    // Check to ensure that input exists. This is important in the case of autofill.
-    if (this.textInputsRefs[id]) this.textInputsRefs[id].focus();
-  }
+	handleEdit(value) {
+		this.setState({ value: value }, () => {
+			console.log("Number", this.state.value);
+		});
+		// App pass a checkPinCode function
 
-  isFocus(id) {
-    let newCode = this.state.code.slice();
+		if (this.props.checkPinCode) {
+			this.props.checkPinCode(value, (success = true) => {
+				// App say it's different than code
+				if (!success) {
+					this.setState({
+						error: this.props.error,
+						code: new Array(this.state.number).fill(""),
+						reset: true
+					});
+				} else {
+					// Is Okay !!!
+					this.props.success();
+					this.setState(prevState => ({
+						code: value,
+						reset: true
+					}));
+				}
+			});
+			return;
+		}
 
-    for (let i = 0; i < newCode.length; i++) if (i >= id) newCode[i] = '';
+		// // no checkPinCode function
+		// // But it's different than code
+		if (this.props.code !== value) {
+			this.setState({
+				error: this.props.error,
+				code: new Array(this.state.number).fill(""),
+				reset: true
+			});
+			return;
+		}
+	}
 
-    this.setState({
-      code: newCode,
-      edit: id
-    });
-  }
+	render() {
+		const {
+			text,
+			success,
+			pinStyle,
+			textStyle,
+			errorStyle,
+			obfuscation,
+			containerStyle,
+			containerPinStyle,
+			passwordText,
+			pinColor,
+			...props
+		} = this.props;
 
-  handleEdit(number, id) {
-    let newCode = this.state.code.slice();
+		const { value, number, reset, edit } = this.state;
 
-    // Detecting if the entire code has been pasted or autofilled into
-    // the first field.
-    const hasAutofilled =
-      number.length > 1 && number.length === this.props.number && id === 0;
+		pins = [];
+		// for (let index = 0; index < this.state.number; index++) {
+		for (let i = 0; i < number; i++) {
+			if (obfuscation) {
+				pins.push(
+					<View
+						key={i}
+						style={{
+							paddingHorizontal: 14,
+							paddingVertical: 18,
+							opacity: i + 1 > value.length ? 0 : 1
+						}}
+					>
+						<SVG width={25} height={25} viewBox="0 0 100 100">
+							<Circle cx="50" cy="50" r="50" fill={pinColor} />
+						</SVG>
+					</View>
+				);
+			} else {
+				pins.push(
+					<View
+						key={i}
+						style={{
+							paddingHorizontal: 14,
+							paddingVertical: 18,
+							opacity: i + 1 > value.length ? 0 : 1
+						}}
+					>
+						<Text>{value[i]}</Text>
+					</View>
+				);
+			}
+		}
 
-    if (hasAutofilled) {
-      newCode = number.split('');
+		const error = this.state.error ? (
+			<Text style={[codePinStyles.error, errorStyle]}>
+				{this.state.error}
+			</Text>
+		) : null;
 
-      // Need to update state so UI updates.
-      this.setState({
-        code: newCode,
-        edit: this.props.number - 1,
-        reset: false
-      });
-    } else {
-      newCode[id] = number[0];
-    }
+		return (
+			<View style={[codePinStyles.container, containerStyle]}>
+				<Text style={[codePinStyles.text, textStyle]}>{text}</Text>
 
-    // User filling the last pin ?
-    if (id === this.state.number - 1 || hasAutofilled) {
-      this.focus(0);
+				{error}
+				<View style={{ opacity: 0, height: 0 }}>
+					<TextInput
+						ref={ref => (this.textInputsRefs = ref)}
+						onChangeText={text => this.handleEdit(text)}
+						value={value}
+						maxLength={number}
+						returnKeyType={"done"}
+						autoCapitalize={"sentences"}
+						autoCorrect={false}
+						autoFocus={true}
+						{...props}
+					/>
+				</View>
 
-      // App pass a checkPinCode function
-      if (this.props.checkPinCode) {
-        this.props.checkPinCode(newCode.join(''), success => {
-          // App say it's different than code
-          if (!success) {
-            this.setState({
-              error: this.props.error,
-              code: new Array(this.state.number).fill(''),
-              edit: 0,
-              reset: true
-            });
-          } else {
-            // Is Okey !!!
-            this.props.success();
-            this.setState(prevState => ({
-              code: newCode,
-              edit: prevState.edit + 1,
-              reset: true
-            }));
-          }
-        });
-
-        return;
-      }
-
-      // no checkPinCode function
-      // But it's different than code
-      if (this.props.code !== newCode.join('')) {
-        this.setState({
-          error: this.props.error,
-          code: new Array(this.state.number).fill(''),
-          edit: 0,
-          reset: true
-        });
-
-        return;
-      }
-
-      this.props.success();
-      this.setState(prevState => ({
-        code: newCode,
-        edit: prevState.edit + 1,
-        reset: true
-      }));
-
-      return;
-    }
-
-    this.focus(this.state.edit + 1);
-
-    this.setState(prevState => {
-      return {
-        error: '',
-        code: newCode,
-        edit: prevState.edit + 1,
-        reset: false
-      };
-    });
-  }
-
-  onKeyPress(e) {
-    if (e.nativeEvent.key === 'Backspace') {
-      const edit = this.state.edit;
-      const toFocus = edit > 0 ? edit - 1 : 0;
-      this.focus(toFocus);
-    }
-  }
-
-  render() {
-    const {
-      text,
-      success,
-      pinStyle,
-      textStyle,
-      errorStyle,
-      obfuscation,
-      containerStyle,
-      containerPinStyle,
-      passwordText,
-      ...props
-    } = this.props;
-
-    pins = [];
-    for (let index = 0; index < this.state.number; index++) {
-      const id = index;
-      const value = this.state.code[id]
-        ? obfuscation
-          ? passwordText
-          : this.state.code[id].toString()
-        : '';
-      pins.push(
-        <TextInput
-          key={id + value + this.state.reset} // force to re-render on update
-          ref={ref => (this.textInputsRefs[id] = ref)}
-          onChangeText={text => this.handleEdit(text, id)}
-          onFocus={() => this.isFocus(id)}
-          value={value}
-          maxLength={id === 0 ? this.props.number : 1}
-          style={[codePinStyles.pin, pinStyle]}
-          returnKeyType={'done'}
-          autoCapitalize={'sentences'}
-          autoCorrect={false}
-          autoFocus={
-            (id === 0 &&
-              this.state.edit === null &&
-              this.props.autoFocusFirst) ||
-            id === this.state.edit
-          }
-          onKeyPress={this.onKeyPress}
-          {...props}
-        />
-      );
-    }
-
-    const error = this.state.error ? (
-      <Text style={[codePinStyles.error, errorStyle]}>{this.state.error}</Text>
-    ) : null;
-
-    return (
-      <View style={[codePinStyles.container, containerStyle]}>
-        <Text style={[codePinStyles.text, textStyle]}>{text}</Text>
-
-        {error}
-
-        <View style={[codePinStyles.containerPin, containerPinStyle]}>
-          {pins}
-        </View>
-      </View>
-    );
-  }
+				<TouchableOpacity onPress={this.focus}>
+					<View
+						style={[
+							codePinStyles.containerPin,
+							containerPinStyle,
+							{
+								borderBottomColor: pinColor,
+								borderBottomWidth: 4
+							}
+						]}
+					>
+						{pins}
+					</View>
+				</TouchableOpacity>
+			</View>
+		);
+	}
 }
 
-CodePin.propTypes = {
-  code: PropTypes.string,
-  success: PropTypes.func.isRequired,
-  number: PropTypes.number,
-  checkPinCode: PropTypes.func,
-  autoFocusFirst: PropTypes.bool,
-  obfuscation: PropTypes.bool,
-  passwordText: PropTypes.string,
-  pinStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  containerPinStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  textStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  errorStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
+PrettyPin.propTypes = {
+	code: PropTypes.string,
+	success: PropTypes.func.isRequired,
+	number: PropTypes.number,
+	checkPinCode: PropTypes.func,
+	autoFocusFirst: PropTypes.bool,
+	obfuscation: PropTypes.bool,
+	passwordText: PropTypes.string,
+	pinColor: PropTypes.string,
+	pinStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+	containerPinStyle: PropTypes.oneOfType([
+		PropTypes.object,
+		PropTypes.number
+	]),
+	containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+	textStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+	errorStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
 };
 
-CodePin.defaultProps = {
-  code: '',
-  number: 4,
-  checkPinCode: null,
-  autoFocusFirst: true,
-  obfuscation: false,
-  passwordText: '*',
-  text: 'Pin code',
-  error: 'Bad pin code.',
-  pinStyle: {},
-  containerPinStyle: {},
-  containerStyle: {},
-  textStyle: {},
-  errorStyle: {}
+PrettyPin.defaultProps = {
+	code: "",
+	number: 4,
+	checkPinCode: null,
+	autoFocusFirst: true,
+	obfuscation: false,
+	passwordText: "*",
+	pinColor: "#000",
+	text: "Pin code",
+	error: "Bad pin code.",
+	pinStyle: {},
+	containerPinStyle: {},
+	containerStyle: {},
+	textStyle: {},
+	errorStyle: {}
 };
 
-export default CodePin;
+export default PrettyPin;
