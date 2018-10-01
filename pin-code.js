b6d86259d163f900f9636b87f0b7e0991eb2c3cd
Worapol Buraphan <xeastcrast@hotmail.com>
2018-10-01 18:17:04 +0700

    fix pin bug

--- pin-code.js
+++ pin-code.js
@@ -1,226 +1,233 @@
 import React, { Component } from "react";
 import { PropTypes } from "prop-types";
 import { TextInput, View, Text, TouchableOpacity } from "react-native";
-import _ from 'lodash'
+import _ from "lodash";
 import Pin from "./pin";
 import { codePinStyles } from "./pin-code-style";
 
 class PrettyPin extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    const codeLength = props.number || props.code.length;
+		const codeLength = props.number || props.code.length;
 
-    this.state = {
-      error: "",
-      number: codeLength,
-      code: new Array(codeLength).fill(""),
-      edit: null,
-      reset: false,
-      value: "",
-      previousLength: 0
-    };
+		this.state = {
+			error: "",
+			number: codeLength,
+			code: new Array(codeLength).fill(""),
+			edit: null,
+			reset: false,
+			value: "",
+			previousLength: 0
+		};
 
-    this.pin = [];
-    this.clean = this.clean.bind(this);
-    this.focus = this.focus.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-  }
+		this.pin = [];
+		this.clean = this.clean.bind(this);
+		this.focus = this.focus.bind(this);
+		this.handleEdit = this.handleEdit.bind(this);
+	}
 
-  componentWillReceiveProps(newProps) {
-    const codeLength = newProps.number || newProps.code.length;
+	componentWillReceiveProps(newProps) {
+		const codeLength = newProps.number || newProps.code.length;
 
-    if (newProps.number !== this.props.number) {
-      this.setState({
-        number: codeLength,
-        edit: null
-      });
-    }
-  }
+		if (newProps.number !== this.props.number) {
+			this.setState({
+				number: codeLength,
+				edit: null
+			});
+		}
+	}
 
-  clean() {
-    this.setState(prevState => {
-      return {
-        code: new Array(prevState.number).fill(""),
-        reset: true,
-        value: "",
-        previousLength: 0
-      };
-    }, this.focus);
+	renderPins() {
+		return Array.apply(null, { length: this.props.number }).map(
+			(value, key) => (
+				<View key={key}>
+					<Pin
+						obfuscation={obfuscation}
+						ref={ref => (this.pin[i] = ref)}
+						pinColor={pinColor}
+						index={i}
+						value={value[i]}
+						styles={codePinStyles.pin}
+					/>
+				</View>
+			)
+		);
+	}
 
-    for (let i = 0; i < this.state.number; i++) {
-      // this.pin[i].hide();
-      setTimeout(this.pin[i].hide, 400);
-    }
-  }
+	clean() {
+		this.setState(prevState => {
+			return {
+				code: new Array(prevState.number).fill(""),
+				reset: true,
+				value: "",
+				previousLength: 0
+			};
+		}, this.focus);
 
-  focus() {
-    // Check to ensure that input exists. This is important in the case of autofill.
-    if (this.textInputsRefs) this.textInputsRefs.focus();
-  }
+		for (let i = 0; i < this.state.number; i++) {
+			// this.pin[i].hide();
+			setTimeout(this.pin[i].hide, 400);
+		}
+	}
 
-  handleEdit(value) {
-    if (value.length > this.state.previousLength) {
-      this.pin[value.length - 1].show();
-    } else {
-      this.pin[this.state.previousLength].hide();
-    }
-    this.setState(
-      {
-        value: value,
-        previousLength: value.length > 0 ? value.length - 1 : 0,
-        error: false
-      },
-      () => {
-        console.log("Value length", value.length);
-        console.log("Previous length", this.state.previousLength);
-      }
-    );
-    // // App pass a checkPinCode function
+	focus() {
+		// Check to ensure that input exists. This is important in the case of autofill.
+		if (this.textInputsRefs) this.textInputsRefs.focus();
+	}
 
-    if (this.props.checkPinCode) {
-      this.props.checkPinCode(value, (success = true) => {
-        // App say it's different than code
-        if (!success) {
-          if (this.props.fail) {
-            this.props.fail();
-          }
-          this.setState({
-            error: this.props.error,
-            code: new Array(this.state.number).fill(""),
-            reset: true
-          });
-        } else {
-          // Is Okay !!!
-          if (this.props.success) {
-            this.props.success();
-          }
-          this.setState(prevState => ({
-            code: value,
-            reset: true
-          }));
-        }
-      });
-      return;
-    }
+	handleEdit(value) {
+		if (value.length > this.state.previousLength) {
+			this.pin[value.length - 1].show();
+		} else {
+			this.pin[this.state.previousLength].hide();
+		}
+		this.setState(
+			{
+				value: value,
+				previousLength: value.length > 0 ? value.length - 1 : 0,
+				error: false
+			},
+			() => {
+				console.log("Value length", value.length);
+				console.log("Previous length", this.state.previousLength);
+			}
+		);
+		// // App pass a checkPinCode function
 
-    // // no checkPinCode function
-    // // But it's different than code
-    if (this.props.code !== value) {
-      this.setState({
-        error: this.props.error,
-        code: new Array(this.state.number).fill(""),
-        reset: true
-      });
-      return;
-    }
-  }
+		if (this.props.checkPinCode) {
+			this.props.checkPinCode(value, (success = true) => {
+				// App say it's different than code
+				if (!success) {
+					if (this.props.fail) {
+						this.props.fail();
+					}
+					this.setState({
+						error: this.props.error,
+						code: new Array(this.state.number).fill(""),
+						reset: true
+					});
+				} else {
+					// Is Okay !!!
+					if (this.props.success) {
+						this.props.success();
+					}
+					this.setState(prevState => ({
+						code: value,
+						reset: true
+					}));
+				}
+			});
+			return;
+		}
 
-  render() {
-    const {
-      text,
-      success,
-      pinStyle,
-      textStyle,
-      errorStyle,
-      obfuscation,
-      containerStyle,
-      containerPinStyle,
-      passwordText,
-      pinColor,
-      ...props
-    } = this.props;
+		// // no checkPinCode function
+		// // But it's different than code
+		if (this.props.code !== value) {
+			this.setState({
+				error: this.props.error,
+				code: new Array(this.state.number).fill(""),
+				reset: true
+			});
+			return;
+		}
+	}
 
-    const { value, number, reset, edit } = this.state;
+	render() {
+		const {
+			text,
+			success,
+			pinStyle,
+			textStyle,
+			errorStyle,
+			obfuscation,
+			containerStyle,
+			containerPinStyle,
+			passwordText,
+			pinColor,
+			...props
+		} = this.props;
 
-    pins = [];
-    // for (let index = 0; index < this.state.number; index++) {
-    for (let i = 0; i < number; i++) {
-      pins.push(
-        <View key={i}>
-          <Pin
-            obfuscation={obfuscation}
-            ref={ref => (this.pin[i] = ref)}
-            pinColor={pinColor}
-            index={i}
-            value={value[i]}
-            styles={codePinStyles.pin}
-          />
-        </View>
-      );
-    }
+		const { value, number, reset, edit } = this.state;
 
-    const error = this.state.error ? (
-      <Text style={[codePinStyles.error, errorStyle]}>{this.state.error}</Text>
-    ) : null;
+		const error = this.state.error ? (
+			<Text style={[codePinStyles.error, errorStyle]}>
+				{this.state.error}
+			</Text>
+		) : null;
 
-    return (
-      <View style={[codePinStyles.container, containerStyle]}>
-        {!_.isEmpty(text) && <Text style={[codePinStyles.text, textStyle]}>{text}</Text>}
-        {error}
-        <TouchableOpacity onPress={this.focus}>
-          <View
-            style={[
-              codePinStyles.containerPin,
-              containerPinStyle,
-              {
-                borderBottomColor: pinColor,
-                borderBottomWidth: 4
-              }
-            ]}
-          >
-            {pins}
-          </View>
-        </TouchableOpacity>
-		<View style={{ opacity: 0, height: 0, margin: 0, padding: 0 }}>
-          <TextInput
-            ref={ref => (this.textInputsRefs = ref)}
-            onChangeText={text => this.handleEdit(text)}
-            value={value}
-            maxLength={number}
-            returnKeyType={"done"}
-            autoCorrect={false}
-            autoFocus={true}
-            {...props}
-          />
-        </View>
-      </View>
-    );
-  }
+		return (
+			<View style={[codePinStyles.container, containerStyle]}>
+				{!_.isEmpty(text) && (
+					<Text style={[codePinStyles.text, textStyle]}>{text}</Text>
+				)}
+				{error}
+				<TouchableOpacity onPress={this.focus}>
+					<View
+						style={[
+							codePinStyles.containerPin,
+							containerPinStyle,
+							{
+								borderBottomColor: pinColor,
+								borderBottomWidth: 4
+							}
+						]}
+					>
+						{this.renderPins()}
+					</View>
+				</TouchableOpacity>
+				<View style={{ opacity: 0, height: 0, margin: 0, padding: 0 }}>
+					<TextInput
+						ref={ref => (this.textInputsRefs = ref)}
+						onChangeText={text => this.handleEdit(text)}
+						value={value}
+						maxLength={number}
+						returnKeyType={"done"}
+						autoCorrect={false}
+						autoFocus={true}
+						{...props}
+					/>
+				</View>
+			</View>
+		);
+	}
 }
 
 PrettyPin.propTypes = {
-  code: PropTypes.string,
-  success: PropTypes.func,
-  fail: PropTypes.func,
-  number: PropTypes.number,
-  checkPinCode: PropTypes.func,
-  autoFocusFirst: PropTypes.bool,
-  obfuscation: PropTypes.bool,
-  passwordText: PropTypes.string,
-  pinColor: PropTypes.string,
-  pinStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  containerPinStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  textStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  errorStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
+	code: PropTypes.string,
+	success: PropTypes.func,
+	fail: PropTypes.func,
+	number: PropTypes.number,
+	checkPinCode: PropTypes.func,
+	autoFocusFirst: PropTypes.bool,
+	obfuscation: PropTypes.bool,
+	passwordText: PropTypes.string,
+	pinColor: PropTypes.string,
+	pinStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+	containerPinStyle: PropTypes.oneOfType([
+		PropTypes.object,
+		PropTypes.number
+	]),
+	containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+	textStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+	errorStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
 };
 
 PrettyPin.defaultProps = {
-  code: "",
-  number: 4,
-  checkPinCode: null,
-  autoFocusFirst: true,
-  obfuscation: false,
-  passwordText: "*",
-  pinColor: "#000",
-  text: "",
-  error: "Bad pin code.",
-  pinStyle: {},
-  containerPinStyle: {},
-  containerStyle: {},
-  textStyle: {},
-  errorStyle: {}
+	code: "",
+	number: 4,
+	checkPinCode: null,
+	autoFocusFirst: true,
+	obfuscation: false,
+	passwordText: "*",
+	pinColor: "#000",
+	text: "",
+	error: "Bad pin code.",
+	pinStyle: {},
+	containerPinStyle: {},
+	containerStyle: {},
+	textStyle: {},
+	errorStyle: {}
 };
 
 export default PrettyPin;
